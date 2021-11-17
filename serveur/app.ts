98d1753fde26e730323a1565f9af488a68f44564
larry109 <69786021+larry109@users.noreply.github.com>
2021-11-17 09:30:06 +0300

    nodets

--- serveur/app.ts
+++ serveur/app.ts
@@ -1,1 +1,10 @@
- 
+require('./config/config.ts');
+const express = require('express');
+var app = express();
+
+
+app.get('/api/:paramID1/:paramID2',function(req, res){
+    return res.json({ A: 'Taskify' });
+});
+
+app.listen(process.env.PORT, () => console.log(`Le server demarre sur le port : ${process.env.PORT}`));
